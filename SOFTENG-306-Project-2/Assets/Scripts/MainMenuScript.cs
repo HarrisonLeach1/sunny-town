41ce8fe11a76299989aebe3feaedcc4b199509c0
Allen <anian100@gmail.com>
2019-10-02 20:54:32 +1300

    change the ordering of menu screens

---
+++ SOFTENG-306-Project-2/Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        Debug.Log("Start game");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Application has quit");
+        Application.Quit();
+    }
+}
