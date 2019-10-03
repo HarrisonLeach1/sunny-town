9c5b6696eee7c59f711cb986e7f321b2374f6b01
William <william.shin.387@gmail.com>
2019-10-03 22:46:17 +1300

    Randomize Minor cards order

--- SOFTENG-306-Project-2/Assets/Scripts/CardFactory.cs
+++ SOFTENG-306-Project-2/Assets/Scripts/CardFactory.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+
 
 public class CardFactory
 {
@@ -12,7 +14,8 @@ public class CardFactory
     {
         reader = new Reader();
         currentPlotCard = reader.RootState;
-        minorCards = new List<MinorCard>(reader.AllMinorStates);
+        minorCards = reader.AllMinorStates;
+        minorCards.Randomize();
     }
 
     public Card GetNewCard(string cardDescriptor)
@@ -30,10 +33,10 @@ public class CardFactory
             case ("minor"):
                 if (minorCards.Count == 0)
                 {
-                    minorCards = new List<MinorCard>(reader.AllMinorStates);
+                    minorCards = reader.AllMinorStates;
+                    minorCards.Randomize();
                 }
-
-                // TODO: Add randomness to card selection
+                
                 var minorCard = minorCards[0];
                 minorCards.Remove(minorCard);
                 return minorCard;
---
+++ SOFTENG-306-Project-2/Assets/Util/Shuffle.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+
+public static class Shuffle
+{
+    private static System.Random rng = new System.Random();  
+
+    public static void Randomize<T>(this IList<T> list)  
+    {  
+        int n = list.Count;  
+        while (n > 1) {  
+            n--;  
+            int k = rng.Next(n + 1);  
+            T value = list[k];  
+            list[k] = list[n];  
+            list[n] = value;  
+        }  
+    }
+}
