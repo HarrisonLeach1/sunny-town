cdf9477f70602c340ea8552737304165c1584a52
William <william.shin.387@gmail.com>
2019-10-04 11:27:36 +1300

    rename variable in Shuffle

--- SOFTENG-306-Project-2/Assets/Util/Shuffle.cs
+++ SOFTENG-306-Project-2/Assets/Util/Shuffle.cs
@@ -3,14 +3,14 @@ using System.Collections.Generic;
 
 public static class Shuffle
 {
-    private static System.Random rng = new System.Random();  
+    private static System.Random random = new System.Random();  
 
     public static void Randomize<T>(this IList<T> list)  
     {  
         int n = list.Count;  
         while (n > 1) {  
             n--;  
-            int k = rng.Next(n + 1);  
+            int k = random.Next(n + 1);  
             T value = list[k];  
             list[k] = list[n];  
             list[n] = value;  
