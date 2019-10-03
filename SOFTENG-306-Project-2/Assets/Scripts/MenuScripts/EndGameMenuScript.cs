1fc897ef0f5f0c2f1741c69cc727d21561cde51e
Allen <anian100@gmail.com>
2019-10-04 08:37:56 +1300

    End game on losing materials

--- SOFTENG-306-Project-2/Assets/Scripts/MenuScripts/EndGameMenuScript.cs
+++ SOFTENG-306-Project-2/Assets/Scripts/MenuScripts/EndGameMenuScript.cs
@@ -1,10 +1,21 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class EndGameMenuScript : MonoBehaviour
 {
+     GameObject screen;
+
+    void Start()
+    {
+        var text = screen.transform.GetChild(0).GetComponent<TextMeshPro>();
+        text.SetText("asdfasdf");
+
+    }
     public void NavigateToMainMenu()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
