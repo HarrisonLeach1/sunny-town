0898ac3942ef2df66ea89c28060985695921e751
William <william.shin.387@gmail.com>
2019-10-06 15:33:35 +1300

    parse simpledialogues from json

--- SOFTENG-306-Project-2/Assets/Scripts/DialogueScripts/DialogueNPC.cs
+++ SOFTENG-306-Project-2/Assets/Scripts/DialogueScripts/DialogueNPC.cs
@@ -1,17 +1,27 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class DialogueNPC : MonoBehaviour
 {
-    public SimpleDialogue dialogue;
+    //public SimpleDialogue dialogue;
+    private Reader reader;
+    private List<SimpleDialogue> expositionDialogues;
 
     private void Start()
     {
+        this.reader = new Reader();
+        this.expositionDialogues = reader.AllExpositionDialogues;
         TriggerDialogue();
     }
     public void TriggerDialogue()
     {
         Action handleDialogueClosed = () => CardManager.Instance.StartDisplayingCards();
-        DialogueManager.Instance.StartExplanatoryDialogue(dialogue, handleDialogueClosed);
+        foreach (SimpleDialogue dialogue in this.expositionDialogues)
+        {
+            DialogueManager.Instance.StartExplanatoryDialogue(dialogue, handleDialogueClosed);
+        }
     }
+    
 }
--- SOFTENG-306-Project-2/Assets/Scripts/DialogueScripts/ExpositionDialogueManager.cs
+++ SOFTENG-306-Project-2/Assets/Scripts/DialogueScripts/ExpositionDialogueManager.cs
@@ -1,4 +1,17 @@
-public class ExpositionDialogueManager
+using System;
+using UnityEngine;
+
+public class ExpositionDialogueManager : MonoBehaviour
 {
-    
+    public SimpleDialogue dialogue;
+
+    private void Start()
+    {
+        TriggerDialogue();
+    }
+    public void TriggerDialogue()
+    {
+        Action handleDialogueClosed = () => CardManager.Instance.StartDisplayingCards();
+        DialogueManager.Instance.StartExplanatoryDialogue(dialogue, handleDialogueClosed);
+    }
 }
