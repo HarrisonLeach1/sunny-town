4db3896635067e22acba45b7850fa78f1e0ce544
Allen <anian100@gmail.com>
2019-10-07 13:47:24 +1300

    Final screen shows game score, also changed to a prefab

--- SOFTENG-306-Project-2/Assets/Scripts/MenuScripts/EndGameMenuScript.cs
+++ SOFTENG-306-Project-2/Assets/Scripts/MenuScripts/EndGameMenuScript.cs
@@ -9,18 +9,27 @@ using UnityEngine.UI;
 public class EndGameMenuScript : MonoBehaviour
 {
 
-    public GameObject winEndScreen;
-    public GameObject lossEndScreen;
+    public GameObject endGameView;
+    public GameObject endGamePrefab;
     
     void Awake()
     {
-        winEndScreen = GameObject.FindGameObjectWithTag("WinEndScreen");
-        lossEndScreen = GameObject.FindGameObjectWithTag("LossEndScreen");
-        var gameEnded = CardManager.Instance.isFinalCard;
-        Debug.Log("Game ended:" + gameEnded);
-        winEndScreen.SetActive(gameEnded);
-        lossEndScreen.SetActive(!gameEnded);
-        
+        endGameView = Instantiate(endGamePrefab, new Vector3(0, 0, 0), Quaternion.identity);
+        var gameScore = endGameView.transform.GetChild(0).GetChild(1).GetChild(2).GetComponent<TextMeshProUGUI>();
+        var gameOutcome = endGameView.transform.GetChild(0).GetChild(1).GetChild(0).GetComponent<TextMeshProUGUI>();
+        var gameBackground = endGameView.transform.GetChild(0).GetChild(0).GetComponent<Image>();
+        gameOutcome.SetText(CardManager.Instance.isFinalCard ? "Game Won" : "Game lost");
+        if (CardManager.Instance.isFinalCard)
+        {
+            gameOutcome.SetText("Game Won");
+            gameBackground.color = new Color32(0, 255, 0, 130);
+        }
+        else
+        {
+            gameOutcome.SetText("Game Lost");
+            gameBackground.color = new Color32(255, 0, 0, 130);
+        }
+        gameScore.SetText("Final Score: " + MetricManager.Instance.GetScore());
     }
     public void NavigateToMainMenu()
     {
