024d901152826289146442df029444b523d705d1
Casey Wong <cwong367@gmail.com>
2019-10-07 22:03:10 +1300

    Additional commenting on exclamation mark scripts

--- SOFTENG-306-Project-2/Assets/Scripts/ExclamationScripts/PopupButtonControllerScript.cs
+++ SOFTENG-306-Project-2/Assets/Scripts/ExclamationScripts/PopupButtonControllerScript.cs
@@ -4,23 +4,26 @@ using UnityEngine;
 
 namespace SunnyTown
 {
-//todo: link up button press with card, using card manager,
-// create the probability factor for the event 
-// using displayminorcard() function 
+    /** Class handles creation and placement of an exclamtion mark button but getting the camera, canvas and location that the button needs to be spawned at
+     */
     public class PopupButtonControllerScript : MonoBehaviour
     {
         private static PopupButtonScript popupButton;
         private static GameObject canvas;
 
-
         public static bool popupShowing;
 
+        /** When initialised class needs to know the canvas object of the game as well as the associated exclamation mark prefab that needs to be spawned 
+         */
         public static void Initialise()
         {
             canvas = GameObject.Find("UI");
             popupButton = Resources.Load<PopupButtonScript>("Prefabs/PopupParent");
         }
 
+        /** Method called in coroutine in the dispatcher class, it handles the creation and positioning of a new exclamation mark button by 
+         ** converting its world position to screen position 
+         */
         public static void CreatePopupButton(Transform location)
         {
             popupShowing = true;
@@ -34,7 +37,9 @@ namespace SunnyTown
             Debug.Log(instance.transform.position);
         }
 
-
+        /** Method called when the exclamation mark fades either after user has clicked on it or after it has timed out
+         ** it resets the boolean maintaining the state of the popup display
+         */
         void DisableExclamationState()
         {
             popupShowing = false;
