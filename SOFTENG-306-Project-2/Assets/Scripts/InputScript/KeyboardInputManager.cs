24a182459d79308469dea0ed5e1453fabf2b6d28
nidhineshnand <nnidhinesh@gmail.com>
2019-10-10 19:07:40 +1300

    Created script for keyboard input

---
+++ SOFTENG-306-Project-2/Assets/Scripts/InputScript/KeyboardInputManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Derived from tutorial found on Board To Bits Games
+public class KeyboardInputManager : InputManager
+{
+    // Events that use delegates
+    public static event MoveInputHandler OnMoveInput;
+    public static event RotateInputHandler OnRotateInput;
+    public static event ZoomInputHandler OnZoomInput;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Movement control transforming
+        if (Input.GetKey(KeyCode.W))
+        {
+            OnMoveInput?.Invoke(Vector3.forward);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            OnMoveInput?.Invoke(-Vector3.forward);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            OnMoveInput?.Invoke(-Vector3.right);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            OnMoveInput?.Invoke(Vector3.right);
+        }
+        
+        // Rotation input
+        if (Input.GetKey(KeyCode.E))
+        {
+            OnRotateInput?.Invoke(1f);
+        }
+        if (Input.GetKey(KeyCode.E))
+        {
+            OnRotateInput?.Invoke(-1f);
+        }
+        
+        // Zoom input
+        if (Input.GetKey(KeyCode.X))
+        {
+            OnZoomInput?.Invoke(1f);
+        }
+        if (Input.GetKey(KeyCode.Z))
+        {
+            OnZoomInput?.Invoke(-1f);
+        }
+    }
+}
