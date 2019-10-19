12c85195d5b8193c6fa3a49bc4f8a02702451cfe
Nidhinesh Nand <nnidhinesh@gmail.com>
2019-10-19 14:01:02 +1300

    Added wind turbines

---
+++ SOFTENG-306-Project-2/Assets/Scripts/WindTurbine/PropellerMovement.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PropellerMovement : MonoBehaviour
+{
+    GameObject[] propellers;
+    private int speed = 0.1;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
