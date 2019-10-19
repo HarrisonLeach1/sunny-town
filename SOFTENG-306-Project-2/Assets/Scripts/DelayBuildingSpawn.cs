d8e70f3ea8b8722b5871e3c27600b20978fdde67
Nidhinesh Nand <nnidhinesh@gmail.com>
2019-10-20 10:45:10 +1300

    Fixed bug that would prevent a delay in the buildings appearing

---
+++ SOFTENG-306-Project-2/Assets/Scripts/DelayBuildingSpawn.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DelayBuildingSpawn : MonoBehaviour
+{
+    public GameObject building;
+    float TmStart;
+    float TmLen=3f;
+ 
+    // Use this for initialization
+    void Start () {
+        TmStart=Time.time;
+        building.SetActive(false);
+    }
+     
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > TmStart + TmLen)
+        {
+
+            building.SetActive(true);
+        }
+
+    }
+}
