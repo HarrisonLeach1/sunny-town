9dc9896307e05c219ee31b51841e871ef170ceba
William <william.shin.387@gmail.com>
2019-10-19 18:01:57 +1300

    add metricboost cheatcode,current cheats do not update metrics bars, story or game progress bar

--- SOFTENG-306-Project-2/Assets/Scripts/CheatScript/CheatsManager.cs
+++ SOFTENG-306-Project-2/Assets/Scripts/CheatScript/CheatsManager.cs
@@ -15,6 +15,7 @@ namespace SunnyTown
         // Update is called once per frame
         void Update()
         {
+            //cheatcode of leveling up
             if (Input.GetKey(KeyCode.L))
             {
                 Debug.Log("Level up cheatcode entered");
@@ -22,6 +23,13 @@ namespace SunnyTown
             }
             
             //cheatcode for maxing out metrics
+            if (Input.GetKey(KeyCode.M))
+            {
+                Debug.Log("Metrics boost cheatcode entered");
+                MetricManager.Instance.UpdateGold(95);
+                MetricManager.Instance.UpdateEnvHealth(95);
+                MetricManager.Instance.UpdatePopHappiness(95);
+            }
         }
     }    
 }
